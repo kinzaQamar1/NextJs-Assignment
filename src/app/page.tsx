7d451c3ddf099f8b37ec0650ed65d42f0ccecce4
kinzaQamar1 <kinzashaheen112@gmail.com>
2024-10-03 12:11:25 +0500

    second assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
-import Girl from "./public/image.jpg";
+import Girl from "./public/image.jpg"
 export default function Home() {
   return (
-    <div>
+    <div className="homeContainer">
+      <div className="childContainer">
+    <h1>
+       <b> HELLO!</b>
+        <br/>
+        My Name Is <span className="color">Kinzaa.</span> 
+        </h1>
+        <p className="small">
+           I am Learning <b>NextJs</b> At <b>GIAIC </b>
+             and I want to become an Excellent Web Developer.
+             </p>
 
-      <div className="homeContainer">
-        <div className="childContainer">
-          <h1>
-            <b> HELLO!</b>
-            <br />
-            My Name Is <span className="color">Kinzaa.</span>
-          </h1>
-          <p className="small">
-            I am Learning <b>NextJs</b> At <b>GIAIC </b>
-            and I want to become an Excellent Web Developer.
-          </p>
-        </div>
-        <div className="picture">
-          <Image src={Girl} alt=" A picture" />
-        </div>
       </div>
+  <div className="picture">
+  <Image src={Girl} alt=" A picture"/>
+       </div>
     </div>
   );
 }
